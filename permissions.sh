f26e48e59afef1fb166052a3e4e139b42aa58e61
Billy Riantono <bernadus@mail.unpas.ac.id>
2020-06-10 18:47:59 +0700

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -5,3 +5,4 @@ sudo find /home/xtreamcodes/iptv_xtream_codes/wwwdir/ -type f -exec chmod 644 {}
 sudo find /home/xtreamcodes/iptv_xtream_codes/wwwdir/ -type d -exec chmod 755 {} \;
 sudo chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx/sbin/nginx
 sudo chmod +x /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
+sudo find /home/xtreamcodes/iptv_xtream_codes/php -type f -exec chmod 644 {} \;
